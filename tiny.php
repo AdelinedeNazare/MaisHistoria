af659f8c1f25aa21e4bba18a9f95696806df61ce
AdelinedeNazare <adelinedenazare@gmail.com>
2016-10-22 19:21:00 -0200

    tinyMCE
    novo modelo de artigo
    
    
    Former-commit-id: 6d4923d65634dc09af13d7879601302ae8c95197

--- tiny.php
+++ tiny.php
@@ -1,14 +1,10 @@
   <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
-    <script>tinymce.init({
+   
+  <script>tinymce.init({
   selector: 'textarea',
   height: 500,
-  plugins: [
-        "advlist autolink lists link image charmap print preview anchor",
-        "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table contextmenu paste imagetools"
-    ],
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-  imagetools_cors_hosts: ['www.tinymce.com', 'codepen.io'],
+  extended_valid_elements: 'img[class=myclass|!src|border:0|alt|title|width|height|style]',
+  invalid_elements: 'strong,b,em,i',
   content_css: [
     '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
     '//www.tinymce.com/css/codepen.min.css'
