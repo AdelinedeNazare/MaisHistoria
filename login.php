a1c5bf13f784fc2d3c930e1f3fa93a0722dcc0bb
AdelinedeNazare <adelinedenazare@gmail.com>
2016-12-21 19:48:46 -0200

    alterando logo
    Former-commit-id: 8b4b72b089a6770444caeb618c3ce8dff34dba65

--- login.php
+++ login.php
@@ -1,20 +1,19 @@
 <div id="cabecalho" class="container"><!--centraliza o layout fixo-->
     <div class="row">
         <div class="col-sm-3">
-            <a href="/#tcc2/"><img src="Imagens/logo.png" alt="" class="img-thumbnail img-responsive"></a>
+            <a href="/#tcc2/"><img src="Imagens/logo_mais historia.png" class=" img-responsive"></a>
         </div>
         <div class="col-sm-3 col-sm-offset-6">
             <div class="panel panel-success">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <?php
+                <div class="panel-heading"><a href="entre.php"> Faça seu login aqui!</a></div>
+                <?php
                     if (!empty($_SESSION['usuario'])) {
                         include 'logado.php';
-                    } else {
+                    } 
+                    else {
                         include 'naologado.php';
                     }
                     ?>
-                </div>
             </div>
         </div>
     </div>
