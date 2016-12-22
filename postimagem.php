fb033b240e6dda2123f045e1149ab610637df51e
AdelinedeNazare <adelinedenazare@gmail.com>
2016-12-21 23:11:09 -0200

    criando pagina do professor
    Former-commit-id: de45373056dbbd5e7a16daed8cb1f002fb2cd27e

--- postimagem.php
+++ postimagem.php
@@ -10,23 +10,39 @@ include_once 'connection.php';
             include_once 'menu.php';
             ?>
         </header>
-        <div class="container">
+        <div class="container conteudo">
             <?php
             include_once 'carrosel.php';
             ?>
-            <div class="row destaque">
-                <h2 style="color:#005858;">Insira uma imagem:</h2>
-                <hr>   
-            </div>    
-            <form action="upload.php" method="post" enctype="multipart/form-data">
-                Selecione uma imagem para upload:
-                <input type="file" name="fileToUpload" id="fileToUpload"> <br>
-                <input type="submit" value="Upload Image" name="submit">
-            </form>        
-            <br><br><br>
+            <div id="gtco-started" style="background-color: #A54812 ">
+                <div class="row animate-box">
+                    <div class="col-md-8 col-md-offset-2 text-center gtco-heading ">
+                        <font color="#D68C2C" >
+                        <h2>Insira sua imagem:</h2>
+                        <hr>  
+                    </div>
+                    <div class="col-sm-4 col-sm-offset-4">
+                        <form class="form-inline" action="" method="post" enctype="multipart/form-data">
+                            <div class="col-md-12 col-sm-12">
+                                <div class="form-group">
+                                    <input type="file" class="form-control" name="imagem" required="">
+                                </div>
+                               <font size="4"> Insira sua descrição sobre a imagem: </font>
+                                  <br> <textarea class="form-control" rows="5" cols="42"></textarea> 
+                            </div>
+                   </div> <br>
+                    <br><div class="col-md-2 col-sm-offset-5">
+                        <Br><button type="submit" name="enviar" class="btn btn-default btn-block"><b>Enviar Imagem</b></button> <br>
+                    </div> 
+                    <div class="col-md-4 col-sm-offset-5">
+                        <a href="postsgeral.php" > <font color="#D68C2C" size="4"> Volte para a página anterior...</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <?php
-        include_once 'footer.php';
-        ?>
-    </body>       
+ <br>        
+<?php
+include_once 'footer.php';
+?> 
+    </body>
 </html>
