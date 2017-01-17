ca23bc6f5afe9d8c2f51cca6865e15a73a52ab92
AdelinedeNazare <adelinedenazare@gmail.com>
2017-01-17 20:22:42 -0200

    adm
    página do adm modificada, designer para aceitar postagem do professor
    
    
    Former-commit-id: d57cb1a1b0c0b37e6e00b1d1a5f02dc3592dbe5b

--- administrativo.php
+++ administrativo.php
@@ -1,5 +1,43 @@
-    <?php
-echo 'Bem vindo, <b> Administrador!</b>';
+<?php
+include_once 'connection.php';
 ?>
-<br/>
-<a href="sair.php">Clique aqui para encerrar sua sessão</a>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <body>
+        <header>
+            <?php
+            include_once 'login.php';
+            include_once 'homeadm.php';
+            ?>
+        </header>
+
+        <div class="container conteudo">
+            <table class="table"  border="3"  >
+                <thead>
+                    <tr  style="background-color: #BC320B" >
+                        <td class="warning" cold-md-1 > <b> ID </b> </td>
+                        <td class="warning" col-md-8> <b> Nome do arquivo </b> </td>
+                        <td class="warning"> <b> Data de submissão </b> </td>
+                        <td class="warning"> <b> Tipo de Arquivo </b> </td>
+                        <td class="warning"> <b> Status</td> </b>
+                    </tr>
+                    <tr  style="background-color: #BC320B" >
+                        <td class="success" cold-md-1>  </td>
+                        <td class="success">  </td>
+                        <td class="success">  </td>
+                        <td class="success">  </td>
+                        <td class="success"> <button class="btn " type="submit"> Aprovar</button> </td> 
+                    </tr>
+                    <tr  style="background-color: #BC320B" >
+                        <td class="success" cold-md-1>  </td>
+                        <td class="success" col-md-8>  </td>
+                        <td class="success">  </td>
+                        <td class="success">  </td>
+                        <td class="success"> </td> 
+                    </tr> 
+                </thead>
+            </table>
+        </div>
+<?php
+include_once 'footer.php';
+?> 
---
+++ homeadm.php
@@ -0,0 +1,20 @@
+<head>
+    <title> MaisHistória </title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap-theme.min.css">       
+    <link rel="stylesheet" href="CSS/style.css" type="text/css"/>
+</head>
+<nav class="navbar navbar-static-top menu">
+    <div class="container">
+        <!--div class="container"-->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </form>
+        </div>
+</nav>
