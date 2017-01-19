0dc1080e905e39ba19414a1ed90e4e04c3015263
AdelinedeNazare <adelinedenazare@gmail.com>
2017-01-19 00:56:05 -0200

    artigo
    menu artigo funcionando
    
    
    Former-commit-id: d5d85cf080fa81f6e600d5ba31633a7f621daaea

--- artigo.php
+++ artigo.php
@@ -10,23 +10,7 @@ include_once 'connection.php';
             include_once 'menu.php';
             ?>
         </header>
-        <?php
-        $id = $_GET["id"];
-        $query = "SELECT * FROM tb_artigos JOIN tb_assunto ON tb_artigos.tb_assunto_idassunto = tb_assunto.idassunto WHERE tb_artigos.id_artigos = $id";
-        $artigo = $conn->query($query);
-
-        if ($artigo->num_rows == 1) {
-            $resultado = $artigo->fetch_assoc();
-            $idProfessor = $resultado['professor_idprofessor'];
-            $queryProfessor = "SELECT * FROM professor WHERE idprofessor = $idProfessor";
-            $professor = $conn->query($queryProfessor)->fetch_assoc();
-        } 
-        else {
-            header("Location: http://localhost/error.php");
-            die();
-        }
-        ?>
-        <div class="container conteudo">
+        <div class="container conteudo">    
             <?php
             include_once 'carrosel.php';
             ?>
@@ -34,21 +18,47 @@ include_once 'connection.php';
                 <h2 style="color:#005858;">Artigos:</h2>
                 <hr>
             </div>
-            <h3 align="middle"> <b><?php echo $resultado['nome']; ?></b></h3>
-            <br />
-            <br />
-            <br />
-            <?php echo $resultado['documento']; ?>
-            <h4 style="color:#005858;"> Por: <?= $professor['nome']; ?></h4>
-            <h4 style="color:#005858;"> Autor(a) do artigo: <?php echo   $resultado['autor_artigo']; ?></h4>
-            <br />
-            <br />
-            <br />
-            <br />
             <?php
-            include_once 'pluggin.php';
-            include_once 'footer.php';
-            ?>
-        </div>
+            //$id = $_GET["id"];
+            $query = "SELECT * FROM tb_artigos JOIN tb_assunto ON tb_artigos.tb_assunto_idassunto = tb_assunto.idassunto;";
+            // $query = "SELECT * FROM tb_artigos, tb_assunto, professor where tb_artigos.tb_assunto_idassunto = tb_assunto.idassunto and professor.idprofessor=tb_artigos.professor_idprofessor";
+            $artigo = $conn->query($query);
+
+            if ($artigo->num_rows) {
+                $tabela = '<table border="1" class="table table-striped">';
+                $tabela .= '<thead>';
+                $tabela .= '<tr> ';
+                $tabela .= '<th class="warning" col-md-1>#ID</th> ';
+                $tabela .= '<th class="warning" col-md-4>Nome do Artigo </th> ';
+                $tabela .= '<th class="warning" col-md-4>Acesse aqui o artigo </th> ';
+                $tabela .= '</tr>';
+                $tabela .= '<tr>';
+
+                foreach ($artigo as $art) {
+                    $resultado = $art; //$artigo->fetch_assoc();
+
+                    $idProfessor = $resultado['professor_idprofessor'];
+                    //$queryProfessor = "SELECT * FROM professor WHERE idprofessor = $idProfessor";
+                    //$professor = $conn->query($queryProfessor)->fetch_assoc();
+
+                    $tabela .= '<td class="info" col-md-1>' . $resultado['id_artigos'] . '</td>';
+                    $tabela .= '<td class="info" col-md-1>' .$resultado['nome'] . '</a></td>'; 
+                    $tabela .= '<td class="info" col-md-1>'.$resultado['imagem']. 'Acesse</a></td>'; 
+                    $tabela .= '</tr>';
+                    $tabela .= ' </thead>';
+                }
+                echo $tabela;
+            } else {
+                header("Location: http://localhost/error.php");
+                die();
+            }
+            ?> 
+
+
+            <br> </div>
+        <?php
+        include_once 'pluggin.php';
+        include_once 'footer.php';
+        ?>
     </body>
 </html>
---
+++ artigomostrar.php
@@ -0,0 +1,53 @@
+<?php
+include_once 'connection.php';
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <body>
+        <header>
+            <?php
+            include_once 'login.php';
+            include_once 'menu.php';
+            ?>
+        </header>
+        <div class="container conteudo">
+            <?php
+            include_once 'carrosel.php';
+            ?>
+            <div class="row destaque">
+                <h2 style="color:#005858;">Artigo:</h2>
+                <hr>
+            </div>
+            <?php
+            $query = "SELECT * FROM tb_artigos JOIN tb_assunto ON tb_artigos.tb_assunto_idassunto = tb_assunto.idassunto";
+        $artigo = $conn->query($query);
+
+        if ($artigo->num_rows >0 ) {
+            $resultado = $artigo->fetch_assoc();
+            $idProfessor = $resultado['professor_idprofessor'];
+            $queryProfessor = "SELECT * FROM professor WHERE idprofessor = $idProfessor";
+            $professor = $conn->query($queryProfessor)->fetch_assoc();
+        } 
+        else {
+            //header("Location: http://localhost/error.php");
+            die();
+        }
+            
+            ?>
+            <h3 align="middle"> <b><?php echo $resultado['nome']; ?></b></h3>
+            <br />  
+            <br /> 
+            <br /> 
+            <?php echo $resultado['documento']; ?>
+            <h4 style="color:#005858;"> Por: <?= $professor['nome']; ?></h4>
+            <h4 style="color:#005858;"> Autor(a) do artigo: <?php echo $resultado['autor_artigo']; ?></h4>
+            <br />
+            <br /> 
+            
+        </div>
+        <?php
+        include_once 'pluggin.php';
+        include_once 'footer.php';
+        ?>
+    </body>
+</html>
