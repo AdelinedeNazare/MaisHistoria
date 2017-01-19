f2ad79fdf729efe2020d63a954af381238f7456f
AdelinedeNazare <adelinedenazare@gmail.com>
2017-01-19 16:45:02 -0200

    imagens
    Former-commit-id: ddf6bc8738dd9ab1318f3affa670c78d3a510c1d

--- imagens.php
+++ imagens.php
@@ -20,7 +20,7 @@ include_once 'connection.php';
             </div>
             <?php
             //$id = $_GET["id"];
-            $query = "SELECT * FROM tb_imagens WHERE tb_imagens.idimagens=5 or tb_imagens.idimagens=1";
+            $query = "SELECT * FROM tb_imagens WHERE tb_imagens.idimagens=tb_imagens.idimagens";
             $imagem = $conn->query($query);
 
             if ($imagem->num_rows) {
@@ -30,6 +30,24 @@ include_once 'connection.php';
                 $nomeImagem = $resultado['nome'];
                 $urlImagem = $resultado['url'];
                 $descricaoImagem = $resultado['descricao'];
+                echo "<font size='6' style='color:#005858;'> " . $nomeImagem;
+                echo "</font>";
+                echo "<br><img src=$urlImagem ";
+                echo "<br><br>" . $descricaoImagem;
+                echo "<br><br>";
+            } else {
+                header("Location: http://localhost/error.php");
+                die();
+            }
+            if ($imagem->num_rows) {
+                $resultado = $imagem->fetch_assoc();
+
+                $idImagem = $resultado['idimagens'];
+                $nomeImagem = $resultado['nome'];
+                $urlImagem = $resultado['url'];
+                $descricaoImagem = $resultado['descricao'];
+
+
                 echo "<font size='6' style='color:#005858;'> " . $nomeImagem;
                 echo "</font>";
                 echo "<br><img src=$urlImagem ";
